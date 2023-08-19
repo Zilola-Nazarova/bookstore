23cdeba2e3849961479ff54a5ba8b969526f78f9
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-08-19 13:25:33 +0500

    Configure routers, create Layout and Navlink components, remove template files

--- src/index.js
+++ src/index.js
@@ -1,20 +1,12 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from 'react-router-dom';
-import './index.css';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <div>Hello world!</div>,
-  },
-]);
+import { BrowserRouter } from 'react-router-dom';
+import App from './components/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );
---
+++ src/routes/Books.js
@@ -0,0 +1,7 @@
+const Books = () => (
+  <div>
+    <h2>Welcome to our Books page!</h2>
+  </div>
+);
+
+export default Books;
