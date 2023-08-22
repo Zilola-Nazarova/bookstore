8ec3ca5dcc1b587bcd75ac4b5b4dc308dca589e7
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-08-23 00:46:24 +0500

    Display BookList by retrieving data from the store

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,29 +1,18 @@
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import { FaTrash } from 'react-icons/fa';
 
-const Book = ({
-  title, author, id, deleteBook,
-}) => (
-  <li>
-    <div>
-      <span>
-        {title}
-      </span>
-      <span>
-        {author}
-      </span>
-      <button type="button" onClick={() => deleteBook(id)}>
-        <FaTrash />
-      </button>
-    </div>
-  </li>
+const Book = () => (
+  <div>
+    A book
+    <FaTrash />
+  </div>
 );
 
-Book.propTypes = {
-  title: PropTypes.string.isRequired,
-  author: PropTypes.string.isRequired,
-  id: PropTypes.string.isRequired,
-  deleteBook: PropTypes.func.isRequired,
-};
+// Book.propTypes = {
+//   title: PropTypes.string.isRequired,
+//   author: PropTypes.string.isRequired,
+//   id: PropTypes.string.isRequired,
+//   deleteBook: PropTypes.func.isRequired,
+// };
 
 export default Book;
