5c4023e344d8516bcde5a04cb271d02ccc95dc3f
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-08-22 19:25:18 +0500

    Fix typo 'stre' to 'store', change the import order (causing ESLint error)

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './components/App';
-import { store } from './redux/store';
 import { Provider } from 'react-redux';
+import { store } from './redux/store';
+import App from './components/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider stre={store}>
+    <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
