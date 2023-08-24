f18dded8985b12290732f8c25e581b6d6740098b
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-08-24 14:09:05 +0500

    Write functions using axios with createAsyncThunk that add/remove book, mutate the response when getBooks.fulfilled

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,28 +1,45 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const url = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/eWTLKRwVwuyoVtssRIYT/books';
+const POST_URL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/eWTLKRwVwuyoVtssRIYT/books';
 
 export const getBooks = createAsyncThunk(
   'books/getBooks',
-  async(_, thunkAPI) => {
+  async (_, thunkAPI) => {
     try {
-      const resp = await axios.get(url, {
-        method: 'POST',
-      });
+      const resp = await axios.get(POST_URL);
       return resp.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error);
     }
-  }
-)
+  },
+);
 
-// function getInitialBooks() {
-//   // getting stored books from local storage
-//   const temp = localStorage.getItem('books');
-//   const savedBooks = JSON.parse(temp);
-//   return savedBooks || booksData;
-// }
+export const createBook = createAsyncThunk(
+  'books/createBook',
+  async (bookObj, thunkAPI) => {
+    try {
+      const resp = await axios.post(POST_URL, bookObj);
+      thunkAPI.dispatch(getBooks());
+      return resp.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  },
+);
+
+export const removeBook = createAsyncThunk(
+  'books/removeBook',
+  async (id, thunkAPI) => {
+    try {
+      const resp = await axios.delete(`${POST_URL}/${id}`, JSON.stringify({ item_id: id }));
+      thunkAPI.dispatch(getBooks());
+      return resp.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  },
+);
 
 const initialState = {
   books: [],
@@ -36,13 +53,9 @@ export const booksSlice = createSlice({
   reducers: {
     addBook: (state, action) => {
       state.books.push(action.payload);
-      // const temp = JSON.stringify(state.books);
-      // localStorage.setItem('books', temp);
     },
     deleteBook: (state, action) => {
       state.books = state.books.filter((book) => book.item_id !== action.payload);
-      // const temp = JSON.stringify(state.books);
-      // localStorage.setItem('books', temp);
     },
   },
   extraReducers(builder) {
@@ -52,12 +65,17 @@ export const booksSlice = createSlice({
       })
       .addCase(getBooks.fulfilled, (state, action) => {
         state.isLoading = false;
-        state.books = action.payload || [];
+        const booksArray = [];
+        Object.keys(action.payload).forEach((bookID) => {
+          action.payload[bookID][0].id = bookID;
+          booksArray.push(action.payload[bookID][0]);
+        });
+        state.books = booksArray;
       })
       .addCase(getBooks.rejected, (state, action) => {
         state.isLoading = false;
-        state.error = action.payload.message
-      })
+        state.error = action.payload.message;
+      });
   },
 });
 
