e238c2b9842bf73c87191d5819ce3bbf003da916
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-08-24 05:09:32 +0500

    Import and dispatch getBooks action in BooksList component, render content conditionally

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -1,18 +1,36 @@
-import { useSelector } from 'react-redux/es/hooks/useSelector';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { getBooks } from '../redux/books/booksSlice';
 import Book from './Book';
 
 const BooksList = () => {
-  const { books } = useSelector((store) => store.books);
+  const dispatch = useDispatch();
+  const { books, isLoading, error } = useSelector((store) => store.books);
+  
+  useEffect(() => {
+    dispatch(getBooks());
+  }, []);
 
   return (
-    <ul>
-      {books.map((book) => (
-        <Book
-          key={book.item_id}
-          book={book}
-        />
-      ))}
-    </ul>
+    <>
+      { isLoading === true ? (
+        <p>The books list is loading!</p>
+      ) : error ? (
+        <p>Something went wrong!
+          <br />
+          { error }
+        </p>
+      ) : (
+        <ul>
+          {books.map((book) => (
+            <Book
+              key={book.item_id}
+              book={book}
+            />
+          ))}
+        </ul>
+      )}
+    </>
   );
 };
 
