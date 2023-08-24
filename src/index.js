0f82ed1bfb2ec8f5f1224446fbae71b026d91f4f
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-08-25 01:10:12 +0500

    Reset CSS for the parent of 'root' component

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import App from './components/App';
+import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
