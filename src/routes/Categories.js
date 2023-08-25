42a34726e3008abe70a645195774c098ad7866f4
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-08-25 18:43:27 +0500

    Style Categories page of the App

--- src/routes/Categories.js
+++ src/routes/Categories.js
@@ -1,18 +1,20 @@
 import { useState } from 'react';
+import styles from '../styles/Categories.module.css';
 
 const Categories = () => {
   const [para, setPara] = useState('');
 
   return (
-    <>
+    <div className={styles.categories_page}>
       <button
+        className={styles.btn}
         type="button"
         onClick={() => setPara('Under construction')}
       >
         check status
       </button>
       <p>{para}</p>
-    </>
+    </div>
   );
 };
 
